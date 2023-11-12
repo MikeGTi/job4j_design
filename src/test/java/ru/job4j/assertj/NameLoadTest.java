fe692834c4189ba1455f0bf59b978647674a5cef
MikeGTi <mikegti@ya.com>
2023-11-12 12:52:54 +0300

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkParseEmptyArrayException() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("array is empty");
+    }
+
+    @Test
+    void checkParseKeyValueWoSymbolEqually() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key:value"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("not contain the symbol '='");
+    }
+
+    @Test
+    void checkParseKeyValueWoKey() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("=value"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a key");
+    }
+
+    @Test
+    void checkParseKeyValueWoValue() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key="))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a value");
+    }
+}
