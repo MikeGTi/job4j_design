e758769c57e6a1497c59b03df330fef22099f868
MikeGTi <mikegti@ya.com>
2023-11-15 16:53:07 +0300

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,63 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class CyclicIterator<T> implements Iterator<T> {
+
+    private final List<T> data;
+    private int index;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (data.size() == 0) {
+            return false;
+        } else {
+            return getNextItemCyclic().isPresent();
+        }
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+
+        Optional<T> rst = getNextItemCyclic();
+        if (rst.isPresent()) {
+            this.index++;
+            return rst.get();
+        } else {
+            throw new Error("Method getNextItem() return 'Optional.empty()' value");
+        }
+    }
+
+    private Optional<T> getNextItemCyclic() {
+        if (data.size() == 0) {
+            return Optional.empty();
+        }
+
+        if (index >= data.size()) {
+            index = 0;
+        }
+
+        for (int i = index; i < data.size(); i++) {
+            if (data.get(i) != null) {
+                this.index = i;
+                return Optional.of(data.get(i));
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
