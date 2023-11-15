e4e9e56000964c585f58a4b9c1eb8dc577ef2fa4
MikeGTi <mikegti@ya.com>
2023-11-15 18:10:52 +0300

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,27 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        Integer item;
+        int nodeIndex = 0;
+        ArrayList<Integer> node;
+
+        while (source.hasNext()) {
+            item = source.next();
+            node = nodes.get(nodeIndex);
+
+            if (item != null) {
+                node.add(item);
+                nodeIndex++;
+                if (nodeIndex == nodes.size()) {
+                    nodeIndex = 0;
+                }
+            }
+        }
+    }
+
+}
