d5bc9b64c1792c205ef5ba9f51959252594e8659
MikeGTi <mikegti@ya.com>
2024-01-07 13:10:07 +0300

    3. Бот [#7921] - corrections

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,9 +3,6 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -17,13 +14,11 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
 
-                    Map<String, List<String>> params = getQueryParams(input.readLine());
-                    String msg = (params.get("msg") != null) ? params.get("msg").get(0).toLowerCase() : "";
-
-                    if (msg.startsWith("exit")) {
+                    String inpReq = input.readLine().toLowerCase();
+                    if (inpReq.contains("msg=bye")) {
                         server.close();
                     } else {
-                        if (msg.startsWith("hello")) {
+                        if (inpReq.contains("hello")) {
                             output.write("Hello!".getBytes());
                         } else {
                             output.write("What?".getBytes());
@@ -33,24 +28,5 @@ public class EchoServer {
                 }
             }
         }
-    }
-
-    private static Map<String, List<String>> getQueryParams(String url) {
-        Map<String, List<String>> params = new HashMap<>();
-        String[] urlParts = url.split("\\?");
-        if (urlParts.length > 1) {
-            String query = urlParts[1];
-            for (String param : query.split("&")) {
-                String[] pair = param.split("=");
-                String key = URLDecoder.decode(pair[0], StandardCharsets.UTF_8);
-                String value = "";
-                if (pair.length > 1) {
-                    value = URLDecoder.decode(pair[1], StandardCharsets.UTF_8);
-                }
-                List<String> values = params.computeIfAbsent(key, k -> new ArrayList<>());
-                values.add(value);
-            }
-        }
-        return params;
     }
 }
