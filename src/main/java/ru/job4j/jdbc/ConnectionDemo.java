9ac26b6849597673e6a4abc564a8edaf0e26cacc
MikeGTi <mikegti@ya.com>
2024-02-26 13:44:15 +0300

    0. JDBC [#6863]

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,33 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.service.Settings;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        Settings settings = new Settings();
+        ClassLoader classLoader = Settings.class.getClassLoader();
+        try (InputStream io = classLoader.getResourceAsStream("app.properties")) {
+            settings.load(io);
+        }
+        Class.forName(settings.getValue("driver"));
+        String url = settings.getValue("url");
+        String login = settings.getValue("login");
+        String password = settings.getValue("password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+
+
+
+}
---
+++ src/test/java/ru/job4j/service/SettingsTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.service;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.InputStream;
+
+class SettingsTest {
+    @Test
+    public void whenKeyDriverThenValueOrgPostgresqlDriver() throws Exception {
+        Settings settings = new Settings();
+        ClassLoader classLoader = Settings.class.getClassLoader();
+        try (InputStream io = classLoader.getResourceAsStream("app.properties")) {
+            settings.load(io);
+        }
+        String value = settings.getValue("driver");
+        assertThat(value).isEqualTo("org.postgresql.Driver");
+    }
+
+    @Test
+    public void whenKeyUrlThenValueDbUrl() throws Exception {
+        Settings settings = new Settings();
+        ClassLoader classLoader = Settings.class.getClassLoader();
+        try (InputStream io = classLoader.getResourceAsStream("app.properties")) {
+            settings.load(io);
+        }
+        String value = settings.getValue("url");
+        assertThat(value).isEqualTo("jdbc:postgresql://localhost:5432/exercises_db");
+    }
+
+    @Test
+    public void whenKeyLoginThenValueUserLogin() throws Exception {
+        Settings settings = new Settings();
+        ClassLoader classLoader = Settings.class.getClassLoader();
+        try (InputStream io = classLoader.getResourceAsStream("app.properties")) {
+            settings.load(io);
+        }
+        String value = settings.getValue("login");
+        assertThat(value).isEqualTo("postgres");
+    }
+
+    @Test
+    public void whenKeyUserThenValueUserLogin() throws Exception {
+        Settings settings = new Settings();
+        ClassLoader classLoader = Settings.class.getClassLoader();
+        try (InputStream io = classLoader.getResourceAsStream("app.properties")) {
+            settings.load(io);
+        }
+        String value = settings.getValue("user");
+        assertThat(value).isNull();
+    }
+
+}
