e24f4051d22a85351e5180eac5f654a9f1ad1706
MikeGTi <mikegti@ya.com>
2024-05-21 12:39:51 +0300

    1. Поддержка форматов XML, JSON в генераторе отчетов. [#851 #497125] - correction

--- src/test/java/ru/job4j/ood/srp/report/ReportJsonTest.java
+++ src/test/java/ru/job4j/ood/srp/report/ReportJsonTest.java
@@ -34,6 +34,25 @@ class ReportJsonTest {
     }
 
     @Test
+    void whenTwoEmployeesReportGenerated() throws JsonParseException {
+        IStore store = new MemoryStore();
+        Employee employee = new Employee("John Doe",
+                new GregorianCalendar(2023, Calendar.JUNE, 8, 17, 41),
+                new GregorianCalendar(2023, Calendar.JUNE, 8, 17, 41),
+                5000.0);
+        Employee employee1 = new Employee("Jane Smith",
+                new GregorianCalendar(2023, Calendar.JUNE, 8, 17, 41),
+                new GregorianCalendar(2023, Calendar.JUNE, 8, 17, 41),
+                6000.0);
+        store.add(employee);
+        store.add(employee1);
+        IReport engine = new ReportJson(store, new GsonBuilder());
+        String expected = """
+        [{"name":"John Doe","hired":"08:06:2023 17:41","fired":"08:06:2023 17:41","salary":5000.0},{"name":"Jane Smith","hired":"08:06:2023 17:41","fired":"08:06:2023 17:41","salary":6000.0}]""";
+        assertThat(engine.generate(em -> true)).isEqualTo(expected);
+    }
+
+    @Test
     void whenSeveralEmployeesReportGenerated() {
         IStore store = new MemoryStore();
         Calendar now = Calendar.getInstance();
@@ -60,37 +79,5 @@ class ReportJsonTest {
         int last = expected.lastIndexOf(",");
         expected.replace(last, last + 1, "]");
         assertThat(itRep.generate(employee -> true)).isEqualTo(expected.toString());
-    }
-
-    @Test
-    void whenAccountantsGenerated() throws JsonParseException {
-        IStore store = new MemoryStore();
-        Employee employee = new Employee("John Doe",
-                new GregorianCalendar(2023, Calendar.JUNE, 8, 17, 41),
-                new GregorianCalendar(2023, Calendar.JUNE, 8, 17, 41),
-                5000.0);
-        Employee employee1 = new Employee("Jane Smith",
-                new GregorianCalendar(2023, Calendar.JUNE, 8, 17, 41),
-                new GregorianCalendar(2023, Calendar.JUNE, 8, 17, 41),
-                6000.0);
-        store.add(employee);
-        store.add(employee1);
-        IReport engine = new ReportJson(store, new GsonBuilder());
-        String expected = """
-                [
-                  {
-                    "name": "John Doe",
-                    "hired": "08:06:2023 17:41",
-                    "fired": "08:06:2023 17:41",
-                    "salary": 5000.0
-                  },
-                  {
-                    "name": "Jane Smith",
-                    "hired": "08:06:2023 17:41",
-                    "fired": "08:06:2023 17:41",
-                    "salary": 6000.0
-                  }
-                ]""";
-        assertThat(engine.generate(em -> true)).isEqualTo(expected);
     }
 }
