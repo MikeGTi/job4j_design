1fcb080adc05d7e53b57102adcd4c83c94ff17d7
MikeGTi <mikegti@ya.com>
2024-05-22 13:00:45 +0300

    0. Принцип подстановки Лисков. [#4915 #497289]

--- src/main/java/ru/job4j/ood/lsp/violators/cars/ToyCar.java
+++ src/main/java/ru/job4j/ood/lsp/violators/cars/ToyCar.java
@@ -25,7 +25,7 @@ public class ToyCar extends Car {
     }
 
     @Override
-    // precondition: 0 < num <= 10
+    /* precondition: 0 < num <= 10 */
     public void doStuff(int num) {
         if (num <= 0 || num > 10) {
             throw new IllegalArgumentException("Input out of range 1-10");
