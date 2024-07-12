1e1ee92a9bc914d6bcd35b269b6b630ed2b47dd1
MikeGTi <mikegti@ya.com>
2024-07-12 21:49:37 +0300

    Тестовое задание "Посетители в банке" [#505164]

--- src/main/java/ru/job4j/algo/sliding/window/StartSliding.java
+++ src/main/java/ru/job4j/algo/sliding/window/StartSliding.java
@@ -1,6 +1,8 @@
 package ru.job4j.algo.sliding.window;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class StartSliding {
     public static int[] findMaxOverlapInterval(List<Interval> intervals) {
@@ -12,48 +14,51 @@ public class StartSliding {
 
         Event[] events = collectEvents(intervals);
         Arrays.sort(events, Event::compareTo);
-        HashMap<Interval, Integer> countMap = countIntersections(events);
 
-        return getMaxIntersectionsInterval(countMap);
+        return getMaxIntersectionsCount(events);
     }
 
-    private static int[] getMaxIntersectionsInterval(HashMap<Interval, Integer> countMap) {
+    /**
+     * Метод выполняет обход массива событий и подсчитывает пересечение по времени
+     * @param events отсортированный по времени массив
+     * @return массив с минимальным диапазоном, с наибольшим количеством пересечений,
+     *         если пересечений нет, возвращает int[-1, -1]
+     */
+    private static int[] getMaxIntersectionsCount(Event[] events) {
+        int start = events[0].time;
+        int end = events[events.length - 1].time;
+        int intersCount;
+        int maxInters = 0;
+        int minSize = 1000;
         int[] rsl = new int[]{-1, -1};
-        if (countMap.size() > 0) {
-            int maxCount = 0;
-            for (Map.Entry<Interval, Integer> entry : countMap.entrySet()) {
-                if (maxCount <= entry.getValue()) {
-                    maxCount = entry.getValue();
-                    rsl[0] = entry.getKey().start;
-                    rsl[1] = entry.getKey().end;
+        for (int i = start; i < end; i++) {
+            if (events[i].isStart) {
+                start = events[i].time;
+                end = 0;
+                intersCount = 0;
+                for (int j = i + 1; j < events[events.length - 1].time; j++) {
+                    if (!events[j].isStart && events[j].time > start) {
+                        end = events[j].time;
+                        break;
+                    }
+                    intersCount++;
+                }
+                if (maxInters <= intersCount && minSize >= end - start) {
+                    maxInters = intersCount;
+                    minSize = end - start;
+                    rsl[0] = start;
+                    rsl[1] = end;
                 }
             }
         }
         return rsl;
     }
 
-    private static HashMap<Interval, Integer> countIntersections(Event[] events) {
-        HashMap<Interval, Integer> countMap = new HashMap<>();
-        int started = events[0].time;
-        int ends = events[events.length - 1].time;
-        int intersCount = 0;
-        for (int i = started; i < ends; i++) {
-            if (events[i].isStart) {
-                started = events[i].time;
-                for (int j = started - 1; j < events.length; j++) {
-                    intersCount++;
-                    if (!events[j].isStart && events[j].time > started) {
-                        ends = events[j].time;
-                        break;
-                    }
-                }
-                countMap.putIfAbsent(new Interval(started, ends), intersCount);
-                intersCount = 0;
-            }
-        }
-        return countMap;
-    }
-
+    /**
+     * Метод выполняет обход массива интервалов и преобразует их в события начала и конца интервала
+     * @param intervals массив объектов Interval
+     * @return не отсортированный массив событий
+     */
     private static Event[] collectEvents(List<Interval> intervals) {
         Event[] events = new Event[intervals.size() * 2];
         int j = 0;
@@ -74,6 +79,6 @@ public class StartSliding {
         intervals.add(new Interval(7, 8));
 
         int[] result = findMaxOverlapInterval(intervals);
-        System.out.printf("Interval that overlaps the maximum number of intervals: [%s, %s%n]", result[0], result[1]);
+        System.out.printf("Minimal size interval that overlaps the maximum number of intervals: [%s, %s]%n", result[0], result[1]);
     }
 }
